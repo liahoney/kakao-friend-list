1b3074acc8b4d91af9cdab771c5be94d61975753
liahoney <toridoc@gmail.com>
2023-01-03 18:54:49 +0900

    data insert

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper'
+
+const statusBarHeight = getStatusBarHeight(true)
+const bottomSpace = getBottomSpace();
+
+console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`)
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Header />
 
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    justifyContent: 'flex-end',
   },
 });
